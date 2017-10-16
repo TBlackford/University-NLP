cb4a05f1e0b5f1dc42059cc2f109682b85b2d649
TBlackford <PAINTA1@student.op.ac.nz>
2017-10-16 17:04:37 +1300

    Starting to add in Gensim routes to API

--- nlp/views.py
+++ nlp/views.py
@@ -1,23 +1,23 @@
-from flask import render_template, make_response, request, send_file, json, jsonify
+from flask import Flask, redirect, url_for, render_template, make_response, request, send_file, json, jsonify
 from flask_restplus import Resource, Namespace
 from nlp import app, api
-import os, random
-from io import BytesIO
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
+import os
+from werkzeug.utils import secure_filename
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+import gensim
+from gensim.models.word2vec import Word2Vec
+from sklearn.manifold import TSNE
+import re
 
 # NLTK
 import nltk
 # Removing this will improve performance maybe
 from nltk.book import *
 
-# Spacy
-import numpy
-import spacy.en
-from collections import Counter
-
 all_books = [text1, text2, text3, text4, text5, text6, text7, text8, text9]
 
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 # This function doesn't work for some reason
 @app.route('/')
@@ -39,6 +39,37 @@ def retrieve_uni_corpus(uni):
 
 ns = Namespace('api', description='API Operations')
 
+### File Upload ###
+
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
+UPLOAD_FOLDER = '/tmp/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        #flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        #flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(SITE_ROOT, 'tmp', filename))
+        return redirect(url_for('index_page',
+                                filename=filename)) # Change the code here somewhere so the
+
+### API Routes ###
+
 @ns.route('/relfreq/<int:uni>/<string:word>')
 # Descriptions of the variables
 @ns.param('uni', 'University')
@@ -50,68 +81,61 @@ class RelativeFrequency(Resource):
         return {word: (freq1[word] / len(uni)) * 100, "corpus": str(uni)}
 
 
-@ns.route('/wordnet/<int:uni>/<string:word>')
-class WordSimilarities(Resource):
-    def get(self, uni, word):
-        pass
-
-
 @ns.route('/universities')
 class Universities(Resource):
     def get(self):
-        SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
         json_url = os.path.join(SITE_ROOT, 'data', 'data.json')
         data = json.load(open(json_url))
         return jsonify(data)
 
-###################################################################################
 
+@ns.route('/wordnet/<int:uni>/<string:word>')
+class WordSimilarities(Resource):
+    def clean(self, stop_words, word):
+        word = word.strip()
+        word = word.lower()
+        word = re.sub('[^A-Za-z0-9]+', '', word)
+        if word not in stop_words:
+            return word
+        else:
+            return ''
 
-ns_img = Namespace('img', "Matplotlib Image Renderer API")
+    def get_sentences(self, file, stopwords):
+        line_count = 0
+        sentences = []
 
+        with open(file, 'r') as inpFile:
+            x = inpFile.readlines()
+            for line in x:
+                if line is not None or line != '\n':
+                    words = line.split()
+                    words = map(lambda x: self.clean(stopwords, x), words)
+                    words = list(filter(lambda x: True if len(x) > 0 else False, words))
+                    sentences.append(words)
 
-@app.route('/plot.png')
-def plot():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
+        return sentences
 
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
+    def get_stopwords(self, stopword_file):
+        stop_words = []
 
-    axis.plot(xs, ys)
-    canvas = FigureCanvas(fig)
-    output = BytesIO()
-    canvas.print_png(output)
-    response = make_response(output.getvalue())
-    response.mimetype = 'image/png'
-    return response
+        with open(stopword_file, 'r') as inpFile:
+            lines = inpFile.readlines()
+            stop_words_temp = map(lambda x: re.sub('\n', '', x), lines)
+            stop_words = list(map(lambda x: re.sub('[^A-Za-z0-9]+', '', x), stop_words_temp))
 
+        return stop_words
 
-@ns_img.route('/plot')
-class ImageAPI(Resource):
-    """API used for RESTful rendering of matplotlib images"""
+    def get(self, uni, word):
+        stopword_file = os.path.join(SITE_ROOT, 'data', 'long_stopwords.txt')
 
-    def get(self):
-        fig = Figure()
-        axis = fig.add_subplot(1, 1, 1)
+        stopwords = self.get_stopwords(stopword_file)
 
-        xs = range(100)
-        ys = [random.randint(1, 50) for x in xs]
+        sentences = self.get_sentences(retrieve_uni_corpus(uni), stopwords)
 
-        axis.plot(xs, ys)
-        canvas = FigureCanvas(fig)
-        output = BytesIO()
-        canvas.print_png(output)
-        response = make_response(output.getvalue())
-        response.mimetype = 'image/png'
-        return response
+        model = Word2Vec(sentences, window=10, size=100, workers=4, min_count=5, hs=1, negative=0)
 
-    @api.marshal_with(int)
-    def post(self, id):
-        print(id)
-        return self.get(self)
+        most_similar = model.most_similar(positive=[word])
 
+        return {word: most_similar}
 
 api.add_namespace(ns)
-api.add_namespace(ns_img)
-
